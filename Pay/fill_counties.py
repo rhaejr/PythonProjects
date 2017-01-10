77ddb3e5ab99f96a92ffb121cb19a36de754631c
rhaejr <av8r.08@gmail.com>
2017-01-10 11:21:28 -0600

    dangit

--- Pay/fill_counties.py
+++ Pay/fill_counties.py
@@ -7,8 +7,8 @@ cur.execute("SELECT * FROM counties where county is null")
 x = cur.fetchall()
 for i in x:
     info = i[1].split(',')
-    # cur.execute('update counties set county="{}" where county_info="{}"'.format(info[3], i[1]))
-    # conn.commit()
+    cur.execute('update counties set county="{}" where county_info="{}"'.format(info[3], i[1]))
+    conn.commit()
 
 
     print(info[3])
