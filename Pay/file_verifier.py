416a3b9efab832fd6b28dc6e71cd039d202fe831
rhaejr <av8r.08@gmail.com>
2017-01-11 09:25:21 -0600

    file verifier

---
+++ Pay/file_verifier.py
@@ -0,0 +1,41 @@
+import csv, sqlite3, re
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+conn = sqlite3.connect('wages.db')
+cur = conn.cursor()
+try:
+    cur.execute('alter table counties add column verified text')
+except sqlite3.OperationalError:
+    pass
+
+select = cur.execute('select wg, wl, ws from counties where verified is NULL').fetchall()
+
+for i in select:
+    verified = False
+    for file in i:
+        print(file)
+        with open(file, 'r',) as f:
+            reader = csv.reader(f)
+            csv_list = list(reader)
+            if len(csv_list) == 15:
+                for row in csv_list:
+                    if len(row) == 5:
+                        row = [cleanhtml(x) for x in row]
+                        row = [float(x) for x in row]
+                        # everything past here should be at least 15 grades and 5 steps
+
+                    else:
+                        cur.execute(
+                            'update counties set verified="Step" where wg="{}" or wl="{}" or ws="{}'.format(file, file,
+                                                                                                             file))
+            else:
+                cur.execute('update counties set verified="Grade" where wg="{}" or wl="{}" or ws="{}"'.format(file, file, file))
+    conn.commit()
+
+
+
+conn.close()
