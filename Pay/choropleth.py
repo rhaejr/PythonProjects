40ca8533a0ef3379a598c83916796db0fb5993e8
rhaejr <av8r.08@gmail.com>
2017-02-08 15:44:55 -0600

    Fisrt Results

--- Pay/choropleth.py
+++ Pay/choropleth.py
@@ -6,14 +6,17 @@ Based on by Mike Bostock's unemployment choropleth http://bl.ocks.org/mbostock/4
 import geoplotlib
 from geoplotlib.utils import BoundingBox
 from geoplotlib.colors import ColorMap
-import json
+import json, math
 import sqlite3, csv
 
-
+scale = 18
+start = 18
 conn = sqlite3.connect('wages.db')
 cur = conn.cursor()
+year = '2016'
+grade = 'wg12'
 
-cur.execute('select date, state, county, wg10 from counties where date like "%2016%"')
+cur.execute('select date, state, county, {} from counties where date like "%{}%"'.format(grade, year))
 counties = cur.fetchall()
 
 with open('GEOIDs.txt', 'r') as f:
@@ -34,27 +37,43 @@ for i in counties:
 test.sort(key=lambda j: float(j[1]))
 
 
-print(test[-1])
+print(test[-1])  # min 18.65  max  35.79 18-36 range is 18
 print(len(counties))
 print(counties[0])
 
 
 # find the unemployment rate for the selected county, and convert it to color
 def get_color(properties):
-    key = str(int(properties['STATE'])) + properties['COUNTY']
+    global start, scale
+    key = str(properties['STATE']) + properties['COUNTY']
     if key in unemployment:
-        return cmap.to_color(unemployment.get(key), 36, 'lin')
+        return cmap.to_color(unemployment.get(key) - start, scale, 'lin')
     else:
         print(key)
         return [0, 0, 0, 0]
 
+def find_min(sorted_list):
+    sorted_list.sort(key=lambda j: float(j[1]))
+    for el in sorted_list:
+        if float(el[1]) != 0:
+            return el[1]
 
+
+def ranger(p_list):
+    p_list.sort(key=lambda j: float(j[1]))
+    low = math.floor(find_min(p_list))
+    high = math.ceil(p_list[-1][1])
+    global scale, start
+    start = low
+    scale = high - low
+    return
+ranger(test)
 unemployment = {t[0]:t[1] for t in test}
 # with open('data/unemployment.json') as fin:
 #     unemployment = json.load(fin)
 
-cmap = ColorMap('Blues', alpha=255, levels=10)
-geoplotlib.geojson('data/gz_2010_us_050_00_20m.json', fill=True, color=get_color, f_tooltip=lambda properties: properties['NAME'])
+cmap = ColorMap('inferno', alpha=255, levels=10)
+geoplotlib.geojson('data/gz_2010_us_050_00_20m.json', fill=True, color=get_color, f_tooltip=(lambda properties: properties['NAME']))
 geoplotlib.geojson('data/gz_2010_us_050_00_20m.json', fill=False, color=[255, 255, 255, 64])
 geoplotlib.set_bbox(BoundingBox.USA)
 geoplotlib.show()
