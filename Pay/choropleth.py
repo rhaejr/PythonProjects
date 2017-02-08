9aec96a2751c6c8379e9858e81a0f56c0a81dac4
rhaejr <av8r.08@gmail.com>
2017-02-08 13:45:05 -0600

    1

--- Pay/choropleth.py
+++ Pay/choropleth.py
@@ -22,10 +22,19 @@ with open('GEOIDs.txt', 'r') as f:
 test = []
 for i in counties:
     for x in csv_list:
-        if i[1] == x[0] and i[2] == x[3]:
-            geoid = str(x[1]) + str(x[2])
-            test.append([geoid, i[3]])
-print(test)
+
+        if i[1] == x[0] and (i[2] in x[3]):
+            geoid = x[1] + x[2]
+            try:
+                test.append([geoid, float(i[3])])
+            except TypeError:
+                test.append([geoid, 0])
+
+
+test.sort(key=lambda j: float(j[1]))
+
+
+print(test[-1])
 print(len(counties))
 print(counties[0])
 
@@ -34,13 +43,15 @@ print(counties[0])
 def get_color(properties):
     key = str(int(properties['STATE'])) + properties['COUNTY']
     if key in unemployment:
-        return cmap.to_color(unemployment.get(key), .15, 'lin')
+        return cmap.to_color(unemployment.get(key), 36, 'lin')
     else:
+        print(key)
         return [0, 0, 0, 0]
 
 
-with open('data/unemployment.json') as fin:
-    unemployment = json.load(fin)
+unemployment = {t[0]:t[1] for t in test}
+# with open('data/unemployment.json') as fin:
+#     unemployment = json.load(fin)
 
 cmap = ColorMap('Blues', alpha=255, levels=10)
 geoplotlib.geojson('data/gz_2010_us_050_00_20m.json', fill=True, color=get_color, f_tooltip=lambda properties: properties['NAME'])
