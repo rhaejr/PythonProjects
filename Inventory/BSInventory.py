737a4b7b24b83a7e3f56572e03c53d6183e3ea62
rhaejr <av8r.08@gmail.com>
2017-02-16 19:19:21 -0600

    starting to look better

--- Inventory/BSInventory.py
+++ Inventory/BSInventory.py
@@ -1,16 +1,38 @@
-import sys
+import sys, sqlite3
 
 from PyQt4 import Qt
 from PyQt4.uic import loadUi
+from gui import Ui_MainWindow
 
+conn = sqlite3.connect('inventory.db')
+cur = conn.cursor()
 
-class Main(Qt.QMainWindow):
+class Main(Qt.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = loadUi("gui.ui", self)
+        self.setupUi(self)
+
+        self.actionLUH.triggered.connect(self.luh_action)
+        self.actionApache.triggered.connect(self.apache_action)
 
 
 
+    def luh_action(self):
+        self.main_label.setText('LUH')
+
+
+    def apache_action(self):
+        self.main_label.setText('Apache')
+        rows = cur.execute('select * from benchstock where acft="apache"').fetchall()
+        self.tableWidget.setRowCount(len(rows))
+        self.tableWidget.setColumnCount(7)
+        row_num = 0
+        for row in rows:
+            column = 0
+            for cell in row:
+                self.tableWidget.setItem(row_num, column, Qt.QTableWidgetItem(str(cell)))
+                column += 1
+            row_num += 1
 
 
 def main():
--- Inventory/database.py
+++ Inventory/database.py
@@ -1,12 +1,28 @@
-import sqlite3
+import sqlite3, csv
 
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('inventory.db')
 cur = conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS users(
-nsn INT PRIMARY KEY      NOT NULL,
-pn    TEXT NOT NULL,
-desc    TEXT NOT NULL,
-remarks    TEXT NOT NULL,
-location TEXT NOT NULL,
-niin TEXT NOT NULL
+cur.execute('''CREATE TABLE IF NOT EXISTS benchstock(
+nsn TEXT     NOT NULL,
+pn    TEXT ,
+desc    TEXT ,
+remarks    TEXT ,
+location TEXT ,
+niin TEXT,
+acft TEXT
 );''')
+
+
+
+with open('inventory.csv', 'r') as f:
+    reader = csv.reader(f)
+    csv_list = list(reader)
+
+for i in csv_list:
+    try:
+        cur.execute('insert into benchstock values(?,"",?,?,?,?,"apache")', (i[1], i[2], i[3], i[0], i[1][4:]))
+    except:
+        print(i)
+
+conn.commit()
+conn.close()
