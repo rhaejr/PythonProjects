ae726af729a2153d3a65adf389d8d7805bdb45a7
rhaejr <av8r.08@gmail.com>
2017-02-21 20:30:02 -0600

    1

--- Youtube/downloader.py
+++ Youtube/downloader.py
@@ -1,6 +1,9 @@
 from pytube import YouTube
-
-yt = YouTube('https://www.youtube.com/watch?v=K-yhef6ZqOw')
+import subprocess
+yt = YouTube('https://www.youtube.com/watch?v=lQ6-nCLeDsI')
 print(yt.get_videos())
 video = yt.get('mp4', '720p')
 video.download('video')
+print('starting audio extraction....')
+subprocess.call('C:\\ffmpeg-20170221-a5c1c7a-win64-static\\bin\\ffmpeg -i "{}.mp4" -vn -c:a copy {}_soundtrack.m4a'.format(yt.filename,yt.filename))
+
