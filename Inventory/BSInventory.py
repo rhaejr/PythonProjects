0ca17e11aee4ebb4b3eca7498d3bfe4de28ba02b
rhaejr <av8r.08@gmail.com>
2017-02-24 16:47:14 -0600

    added update

--- Inventory/BSInventory.py
+++ Inventory/BSInventory.py
@@ -21,6 +21,12 @@ class Main(Qt.QMainWindow, Ui_MainWindow):
         self.ui.actionApache.triggered.connect(self.apache_action)
         self.ui.add_button.clicked.connect(self.add_item)
         self.ui.search_button.clicked.connect(self.search)
+        self.ui.select_row_button.clicked.connect(self.load_from_table)
+        self.ui.update_button.clicked.connect(self.update_db)
+        self.ui.tableWidget.doubleClicked.connect(self.load_from_table)
+        self.ui.tableWidget.setEditTriggers(Qt.QAbstractItemView.NoEditTriggers)
+        self.header = self.ui.tableWidget.horizontalHeader()
+        self.header.setStretchLastSection(True)
 
 
 
@@ -41,9 +47,13 @@ class Main(Qt.QMainWindow, Ui_MainWindow):
             row_num += 1
 
         self.ui.tableWidget.setHorizontalHeaderLabels(
-            ['pn', 'location', 'description', 'remarks'])
+            ['PN', 'LOCATION', 'DESCRIPTION', 'REMARKS'])
+        self.ui.tableWidget.resizeColumnsToContents()
 
 
+        # header = self.ui.tableWidget.horizontalHeader()
+        # header.setStretchLastSection(True)
+        # self.ui.tableWidget.resizeColumnsToContents()
     def apache_action(self):
         self.ui.main_label.setText('Apache')
         self.acft = 'apache'
@@ -60,7 +70,12 @@ class Main(Qt.QMainWindow, Ui_MainWindow):
             row_num += 1
 
         self.ui.tableWidget.setHorizontalHeaderLabels(
-            ['nsn', 'pn', 'niin', 'location', 'description', 'remarks'])
+            ['NSN', 'PN', 'NIIN', 'LOCATION', 'DESCRIPTION', 'REMARKS'])
+        self.ui.tableWidget.resizeColumnsToContents()
+
+
+
+
 
     # nsn, pn, desc, remarks, location, niin, acft
     def add_item(self):
@@ -86,6 +101,42 @@ class Main(Qt.QMainWindow, Ui_MainWindow):
                 self.apache_action()
             else:
                 self.luh_action()
+
+    def load_from_table(self):
+        rowID = self.ui.tableWidget.currentRow()
+        items = []
+
+        if len(self.ui.tableWidget.selectedItems()) == self.ui.tableWidget.columnCount():
+
+            items = self.ui.tableWidget.selectedItems()
+            for n in range(len(items)):
+                items[n] = items[n].text()
+        else:
+            for i in range(self.ui.tableWidget.columnCount()):
+                items.append(self.ui.tableWidget.item(rowID, i).text())
+        self.ui.nsn_edit.setText(items[0])
+        self.ui.pn_edit.setText(items[1])
+        self.ui.niin_edit.setText(items[2])
+        self.ui.loc_edit.setText(items[3])
+        self.ui.desc_edit.setText(items[4])
+        self.ui.remarks_edit.setText(items[5])
+
+    def update_db(self):
+        fields = [[self.ui.nsn_edit.text(), 'nsn'],
+                  [self.ui.niin_edit.text(), 'niin'],
+                  [self.ui.pn_edit.text(), 'pn'],
+                  [self.ui.desc_edit.text(), 'desc'],
+                  [self.ui.loc_edit.text(), 'location'],
+                  [self.ui.remarks_edit.text(), 'remarks']]
+        cur.execute('update benchstock set nsn="{}", niin="{}", pn="{}", desc="{}", location="{}", remarks="{}" where nsn="{}"'.format(
+            fields[0][0], fields[1][0], fields[2][0], fields[3][0], fields[4][0], fields[5][0], fields[0][0]))
+
+        conn.commit()
+        if self.acft == 'apache':
+            self.apache_action()
+        else:
+            self.luh_action()
+
 
 
 
