5fb878933b2968fe97435a1b89fc26573995b8d5
rhaejr <av8r.08@gmail.com>
2017-04-26 10:18:46 -0500

    fixing pages and messed with barcode positioning

--- Inventory/label_maker.py
+++ Inventory/label_maker.py
@@ -20,31 +20,43 @@ registerFont(TTFont('3of9', os.path.join(font_path, 'free3of9.ttf')))
 # Create an A4 portrait (210mm x 297mm) sheets with 2 columns and 8 rows of
 # labels. Each label is 90mm x 25mm with a 2mm rounded corner. The margins are
 # automatically calculated.
-specs = labels.Specification(210, 297, columns, rows, label_width, label_height,  top_padding=0, bottom_padding=0,
+# 215.9 by 279.4 mm
+specs = labels.Specification(215.9, 279.4, columns, rows, label_width, label_height,  top_padding=0, bottom_padding=0,
                              row_gap=0, corner_radius=2)
 
 # Create a function to draw each label. This will be given the ReportLab drawing
 # object to draw on, the dimensions (NB. these will be in points, the unit
 # ReportLab uses) of the label, and the object to render.
-def draw_label(label, width, height, obj):
+def draw_label_top_bc(label, width, height, obj):
     # Just convert the object to a string and print this at the bottom left of
     # the label.
-    # label.add(10,2,shapes.Image(obj[0]))
-    label.add(shapes.String(2, 2, str(obj[0]), fontName="Helvetica", fontSize=14))
-    label.add(shapes.String(2, 16, str(obj[1]), fontName="Helvetica", fontSize=14 ))
-    label.add(shapes.String(2, 28, str(obj[2]), fontName="Helvetica", fontSize=14))
-    # print(obj[3])
-    label.add(shapes.Image(2, 55, 180, 40, make_barcode(obj[3])))
+    label.add(shapes.String(2, 2, str(obj[0]), fontName="Helvetica", fontSize=12))
+    label.add(shapes.String(2, 13, str(obj[1]), fontName="Helvetica", fontSize=12 ))
+    label.add(shapes.String(2, 24, str(obj[2]), fontName="Helvetica", fontSize=12))
+    label.add(shapes.Image(2, 46, 180, 40, make_barcode(obj[3])))
     # label.add(shapes.String(38, 30, str(obj[3]), fontName="3of9", fontSize=30))
-    # label.add(shapes.String(2, 55, str(obj[4]), fontName="Helvetica", fontSize=14))
+    label.add(shapes.String(2, 35, str(obj[4]), fontName="Helvetica", fontSize=12))
+
+# def draw_label_btm_bc(label, width, height, obj):
+#     # Just convert the object to a string and print this at the bottom left of
+#     # the label.
+#     label.add(shapes.String(2, 2, str(obj[0]), fontName="Helvetica", fontSize=14))
+#     label.add(shapes.String(2, 16, str(obj[1]), fontName="Helvetica", fontSize=14 ))
+#     label.add(shapes.String(2, 28, str(obj[2]), fontName="Helvetica", fontSize=14))
+#     label.add(shapes.Image(2, 55, 180, 40, make_barcode(obj[3])))
+#     # label.add(shapes.String(38, 30, str(obj[3]), fontName="3of9", fontSize=30))
+#     # label.add(shapes.String(2, 55, str(obj[4]), fontName="Helvetica", fontSize=14))
+
 
 def make_barcode(code):
     code = str(code)
+    if len(code) > 9:
+        code = code[-9:]
     ean = barcode.get('ean13', code, writer=ImageWriter())
-    filename = ean.save('barcodes/{}'.format(code))
+    filename = ean.save('barcodes/{}'.format(code),{'font_size':14, 'text_distance':2})
     return filename
 # Create the sheet.
-sheet = labels.Sheet(specs, draw_label, border=True)
+sheet = labels.Sheet(specs, draw_label_top_bc, border=True)
 # trying to make barcode
 # ean = barcode.get('ean13', '1234121231234', writer=ImageWriter())
 # filename = ean.save('ean13')
