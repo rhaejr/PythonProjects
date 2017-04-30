c45b1cfbee0398f6fbbd8aa91046dcd1b8179189
rhaejr <av8r.08@gmail.com>
2017-04-30 15:06:12 -0500

    Revert "1"
    This reverts commit 9715ccbdb97357207d57162a7d4a55d6fb45781e.

--- Inventory/label_maker.py
+++ Inventory/label_maker.py
@@ -9,12 +9,13 @@ from barcode.writer import ImageWriter
 conn = sqlite3.connect('inventory.db')
 cur = conn.cursor()
 # nsn, pn, desc, remarks, location, niin, acft
-db = cur.execute('select desc, pn, location, niin, nsn, acft from benchstock where acft="apache"').fetchall()
+db = cur.execute('select desc, pn, location, nsn, nsn from benchstock where acft="apache"').fetchall()
 
 font_path = 'fonts'
 big_lablels = (101, 34, 2, 7)
 address_labels = (68.675, 26.4, 3, 10)
 label_width , label_height, columns, rows = address_labels
+
 registerFont(TTFont('3of9', os.path.join(font_path, 'free3of9.ttf')))
 # Create an A4 portrait (210mm x 297mm) sheets with 2 columns and 8 rows of
 # labels. Each label is 90mm x 25mm with a 2mm rounded corner. The margins are
@@ -32,7 +33,7 @@ def draw_label_top_bc(label, width, height, obj):
     label.add(shapes.String(2, 2, str(obj[0]), fontName="Helvetica", fontSize=12))
     label.add(shapes.String(2, 13, str(obj[1]), fontName="Helvetica", fontSize=12 ))
     label.add(shapes.String(2, 24, str(obj[2]), fontName="Helvetica", fontSize=12))
-    label.add(shapes.Image(2, 46, 180, 40, make_barcode(obj[3], obj[5])))
+    label.add(shapes.Image(2, 46, 180, 40, make_barcode(obj[3])))
     # label.add(shapes.String(38, 30, str(obj[3]), fontName="3of9", fontSize=30))
     label.add(shapes.String(2, 35, str(obj[4]), fontName="Helvetica", fontSize=12))
 
@@ -47,15 +48,12 @@ def draw_label_top_bc(label, width, height, obj):
 #     # label.add(shapes.String(2, 55, str(obj[4]), fontName="Helvetica", fontSize=14))
 
 
-def make_barcode(code, acft):
-    leading = '000'
-    if acft == 'luh':
-        leading = '720'
-    elif acft == 'apache':
-        leading = '640'
-    code = leading + str(code)
+def make_barcode(code):
+    code = str(code)
+    if len(code) > 9:
+        code = code[-9:]
     ean = barcode.get('ean13', code, writer=ImageWriter())
-    filename = ean.save('barcodes/{}'.format(code))
+    filename = ean.save('barcodes/{}'.format(code),{'font_size':14, 'text_distance':2})
     return filename
 # Create the sheet.
 sheet = labels.Sheet(specs, draw_label_top_bc, border=False)
@@ -67,11 +65,11 @@ sheet = labels.Sheet(specs, draw_label_top_bc, border=False)
 # Add a couple of labels.
 s = '1234121231234'
 n = '{}-{}-{}-{}'.format(s[:-9],s[-9:-7],s[-7:-4],s[-4:])
-# sheet.add_label([ 'Nomenclature','Part No.', 'Location', '1234121231234',n, ''])
+sheet.add_label([ 'Nomenclature','Part No.', 'Location', '1234121231234',n])
 for row in db:
     if row[3] != '':
         nsn = '{}-{}-{}-{}'.format(row[4][:-9],row[4][-9:-7],row[4][-7:-4],row[4][-4:])
-        sheet.add_label([row[0],row[1],row[2],row[3],nsn, row[5]])
+        sheet.add_label([row[0],row[1],row[2],int(row[3]),nsn])
 # sheet.add_label("Hello")
 # sheet.add_label("World")
 
