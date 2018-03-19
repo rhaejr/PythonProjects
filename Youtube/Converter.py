a4a62a71fab788d144488b93164d6dfd433a7665
rhaejr <av8r.08@gmail.com>
2018-03-18 21:39:49 -0500

    1

--- Youtube/Converter.py
+++ Youtube/Converter.py
@@ -3,3 +3,5 @@ title = 'Crushing Steel Toe Cap Boots with Hydraulic Press!'
 ff = FFmpeg(inputs={'video/{}.mp4'.format(title): None}, outputs={'video/{}Converted.mpeg'.format(title): None})
 
 ff.run()
+
+import
--- Youtube/downloader.py
+++ Youtube/downloader.py
@@ -1,9 +1,22 @@
 from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=HZTgmC0Pmb8')
-print(yt.get_videos())
 
-video = yt.get('mp4', '{}p'.format(input('resolution: ')))
-video.download('video')
+videos = ['https://www.youtube.com/watch?v=8OnitO8wS6A',
+          'https://www.youtube.com/watch?v=Nex2B7yFxYs',
+          'https://www.youtube.com/watch?v=aZyUO5shXZg',
+          'https://www.youtube.com/watch?v=1fhdHPBqUa8']
+
+for i in videos:
+    try:
+        YouTube(i).streams.first().download()
+        print('Finished')
+    except Exception as e:
+        print(e)
+
+# yt = YouTube('https://www.youtube.com/watch?v=HZTgmC0Pmb8')
+# print(yt.get_videos())
+#
+# video = yt.get('mp4', '{}p'.format(input('resolution: ')))
+# video.download('video')
 # print('starting audio extraction....')
 # subprocess.call('C:\\ffmpeg-20170221-a5c1c7a-win64-static\\bin\\ffmpeg -i "{}.mp4" -vn -c:a copy {}_soundtrack.m4a'.format(yt.filename,yt.filename))
 
